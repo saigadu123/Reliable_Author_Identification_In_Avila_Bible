32bfa565b37bc6a9b6b7c6b2fa8bc86e3c93bccb
saikrishna <saikrishnagorantla2001@gmail.com>
2022-06-28 08:55:47 +0530

    update2

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from Avila.logger import logging
+from Avila.exception import AvilaException
+import sys
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return "Avila-Bible project"
+    try:
+       raise Exception("We are raising custom Exception")
+    except Exception as e:
+        avila = AvilaException(e,sys)
+        logging.info(avila.error_message)
+
+    logging.info("My own logger project")
+    return "Avila-Bible project ineuron internship"
 
 
 if __name__ == "__main__":
