f8e190b9dd489f7954b941778536f28c6ff750f0
saikrishna <saikrishnagorantla2001@gmail.com>
2022-06-28 13:25:19 +0530

    Configuration done

---
+++ Avila/util/util.py
@@ -0,0 +1,13 @@
+import sys
+import os
+import yaml
+from Avila.exception import AvilaException
+
+def read_yaml_file(file_path:str)->dict:
+    
+    try:
+        with open(file_path,'rb') as yaml_file:
+            yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise AvilaException(e,sys) from e
+
