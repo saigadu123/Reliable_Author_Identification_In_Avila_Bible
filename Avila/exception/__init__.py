94866af1418c451f51308485c4d4bb67d8c9d7c1
saikrishna <saikrishnagorantla2001@gmail.com>
2022-06-28 10:05:36 +0530

    update

--- Avila/exception/__init__.py
+++ Avila/exception/__init__.py
@@ -5,7 +5,7 @@ class AvilaException(Exception):
 
     def __init__(self,error_message:Exception,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = self.get_detailed_error_message(error_message=error_message,error_detail=error_detail)
+        self.error_message = AvilaException.get_detailed_error_message(error_message=error_message,error_detail=error_detail)
 
     @staticmethod
     def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
